34ddf61da95d23d2efc6a4c720917d007a6ed420
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:42:39 +0800

    Update kanye_rest.py

--- src/kanye_rest.py
+++ src/kanye_rest.py
@@ -1,23 +1,22 @@
 from requests import get
 
-
 class KanyeRest:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.kanye.rest"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
-	def get_random_quote(self):
+	def get_random_quote(self) -> dict:
 		return get(
 			self.api, headers=self.headers).json()
 
-	def get_random_quote_as_text(self):
+	def get_random_quote_as_text(self) -> str:
 		return get(
 			f"{self.api}/text",
 			headers=self.headers).text
 
-	def get_all_quotes(self):
+	def get_all_quotes(self) -> dict:
 		return get(
 			"https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json",
 			headers=self.headers).json()
